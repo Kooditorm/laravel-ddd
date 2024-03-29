c8974ad5fd5a71762a32f523472ec6eb3854bfaf
胡伟涛 <506711663@qq.com>
2024-03-29 18:00:53 +0800

    初始化项目

--- src/DDDCoreServiceProvider.php
+++ src/DDDCoreServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace DDDCore;
 
-use DDDCore\Console\Commands\CronTabCommand;
-use DddCore\Console\Makers\MakerCommand;
 use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 
@@ -27,15 +25,7 @@ class DDDCoreServiceProvider extends ServiceProvider implements DeferrableProvid
      */
     public function register(): void
     {
-        $this->app->singleton('command.ddd-core.crontab', function () {
-            return new CronTabCommand();
-        });
-        $this->app->singleton('command.ddd-core.maker', function () {
-            return new MakerCommand();
-        });
-        $this->commands(
-            'command.ddd-core.crontab'
-        );
+
     }
 
 
@@ -46,6 +36,6 @@ class DDDCoreServiceProvider extends ServiceProvider implements DeferrableProvid
      */
     public function provides(): array
     {
-        return ['command.ddd-core.crontab', 'command.ddd-core.maker'];
+        return [];
     }
 }
---
+++ src/Libraries/Laravel/Contracts/Console/Kernel.php
@@ -0,0 +1,8 @@
+<?php
+namespace DddCore\Libraries\Laravel\Contracts\Console;
+
+use Illuminate\Contracts\Console\Kernel as LaravelKernel;
+interface Kernel extends LaravelKernel
+{
+
+}
