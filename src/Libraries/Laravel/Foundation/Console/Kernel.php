1d55c416004c95c9bc5b4be643e5350b3f7fb227
胡伟涛 <506711663@qq.com>
2024-04-01 08:59:42 +0800

    初始化项目

---
+++ src/Libraries/Laravel/Foundation/Console/Kernel.php
@@ -0,0 +1,46 @@
+<?php
+namespace DddCore\Libraries\Laravel\Foundation\Console;
+
+use Illuminate\Console\Application as Artisan;
+use Illuminate\Console\Command;
+use Illuminate\Foundation\Console\Kernel as LaravelKernel;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use ReflectionClass;
+use Symfony\Component\Finder\Finder;
+
+class Kernel extends LaravelKernel
+{
+
+
+    protected function load($paths)
+    {
+        $paths = array_unique(Arr::wrap($paths));
+
+        $paths = array_filter($paths, function ($path) {
+            return is_dir($path);
+        });
+
+        if (empty($paths)) {
+            return;
+        }
+
+        $namespace = $this->app->getNamespace();
+
+        foreach ((new Finder)->in($paths)->files() as $command) {
+            $command = $namespace.str_replace(
+                    ['/', '.php'],
+                    ['\\', ''],
+                    Str::after($command->getRealPath(), realpath(app_path()).DIRECTORY_SEPARATOR)
+                );
+
+            if (is_subclass_of($command, Command::class) &&
+                ! (new ReflectionClass($command))->isAbstract()) {
+                Artisan::starting(function ($artisan) use ($command) {
+                    $artisan->resolve($command);
+                });
+            }
+        }
+    }
+
+}
