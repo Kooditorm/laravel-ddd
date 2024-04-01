e9dac70aa99c20d8a605486d0cd0133aaa6d096b
胡伟涛 <506711663@qq.com>
2024-04-01 10:22:34 +0800

    初始化项目

---
+++ src/Console/Kernel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace DDDCore\Console;
+
+usE \DddCore\Libraries\Laravel\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+
+}
--- src/Libraries/Laravel/Contracts/Console/Kernel.php
+++ src/Libraries/Laravel/Contracts/Console/Kernel.php
@@ -2,6 +2,7 @@
 namespace DddCore\Libraries\Laravel\Contracts\Console;
 
 use Illuminate\Contracts\Console\Kernel as LaravelKernel;
+
 interface Kernel extends LaravelKernel
 {
 
