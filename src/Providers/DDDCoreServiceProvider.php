68ab28727cd5ed48b8550cac880fabec8e15cdf0
胡伟涛 <506711663@qq.com>
2024-04-19 14:27:54 +0800

    优化DTO 核心功能

--- src/Providers/DDDCoreServiceProvider.php
+++ src/Providers/DDDCoreServiceProvider.php
@@ -14,6 +14,7 @@ class DDDCoreServiceProvider extends AggregateServiceProvider implements Deferra
      * @var string[]
      */
     protected $providers = [
+        ListenDBServiceProvider::class,
         ArtisanServiceProvider::class
     ];
 }
