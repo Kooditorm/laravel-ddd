3277bb36a0f56668f30ad43110361ae5b89c2998
胡伟涛 <506711663@qq.com>
2024-04-19 17:41:42 +0800

    优化DTO 核心功能

--- src/Repositories/BaseRepository.php
+++ src/Repositories/BaseRepository.php
@@ -30,6 +30,7 @@ abstract class BaseRepository extends RootRepository implements BaseRepositoryIn
         $this->model->setTable($table);
         return $this;
     }
+
     /**
      * Boot up the repository, pushing criteria
      * @throws RepositoryException
@@ -37,5 +38,53 @@ abstract class BaseRepository extends RootRepository implements BaseRepositoryIn
     public function boot(): void
     {
         $this->pushCriteria(app(RequestCriteria::class));
+    }
+
+    /**
+     * @inheritDoc
+     * @throw JSONException
+     */
+    public function getSuperior(array $where, array $fields = [], string $childFiled = 'id', string $parentField = 'parent_id'): array
+    {
+        $rtn = [];
+
+        if (!empty($where)) {
+            $field = array_merge([$childFiled, $parentField], $fields);
+
+            $field = collect($field)->each(function ($item) {
+                return 'tab.'.$item;
+            })->toArray();
+
+            $field = implode(',', $field);
+
+            $whereArr = [];
+            foreach ($where as $k => $v) {
+                if (is_string($k)) {
+                    $whereArr[] = 'tab.'.$k.' = '.$v;
+                } elseif (is_numeric($k)) {
+                    $whereArr[] = 'tab.'.$v[0].' '.$v[1].' '.$v[2];
+                }
+            }
+
+            $whereStr = implode(' and ', $whereArr);
+
+            if (!empty($whereStr)) {
+                $whereStr = 'where '.$whereStr;
+            }
+
+            $table = $this->model->getTable();
+            $sql   = " with recursive _sup as ";
+            $sql   .= " (select {$field} from {$table} tab {$whereStr} union all ";
+            $sql   .= " select {$field} from _sup,{$table} tab where tab.{$childFiled} = _sup.{$parentField}) select * from _sup";
+
+            $res = DB::select($sql);
+
+            if (!empty($res)) {
+                $rtn = json_decode(json_encode($res, JSON_THROW_ON_ERROR), true, 512, JSON_THROW_ON_ERROR);
+            }
+        }
+
+
+        return $rtn;
     }
 }
--- src/Repositories/BaseRepositoryInterface.php
+++ src/Repositories/BaseRepositoryInterface.php
@@ -5,4 +5,14 @@ namespace DDDCore\Repositories;
 interface BaseRepositoryInterface
 {
 
+    /**
+     * 获取上下级数据
+     *
+     * @param  array  $where
+     * @param  array  $fields
+     * @param  string  $childFiled
+     * @param  string  $parentField
+     * @return array
+     */
+    public function getSuperior(array $where, array $fields, string $childFiled, string $parentField): array;
 }
