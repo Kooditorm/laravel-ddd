8707f10401c7ff5096536f7615fb7ed797c7bf7f
胡伟涛 <506711663@qq.com>
2024-04-22 16:42:52 +0800

    重写验证类

--- src/Listeners/AsyncEventListener.php
+++ src/Listeners/AsyncEventListener.php
@@ -5,6 +5,12 @@ namespace DDDCore\Listeners;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
+/**
+ * 监听异步event事件
+ *
+ * @class AsyncEventListener
+ * @package DDDCore\Listeners
+ */
 class AsyncEventListener extends EventListener implements ShouldQueue
 {
     use InteractsWithQueue;
---
+++ src/Listeners/SyncEventListener.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace DDDCore\Listeners;
+
+/**
+ * 监听同步event事件
+ *
+ * @class SyncEventListener
+ * @package DDDCore\Listeners
+ */
+class SyncEventListener extends EventListener
+{
+
+}
