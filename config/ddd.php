f66614e6568f03733fe0da78355e28e4ddcb9fd7
小黑 <506711663@qq.com>
2024-05-08 10:46:04 +0800

    修改脚本名称

---
+++ config/ddd.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'application' => [
+        "domain_name" => env('DOMAIN_NAME', 'Domains'),
+        'interface_name' => env('INTERFACE_NAME', 'Interfaces'),
+    ],
+
+    'event' => [],
+
+];
--- src/Console/Commands/InitCommand.php
+++ src/Console/Commands/InitCommand.php
@@ -2,6 +2,8 @@
 
 namespace DDDCore\Console\Commands;
 
+use RuntimeException;
+
 class InitCommand extends BaseCommand
 {
 
@@ -31,6 +33,52 @@ class InitCommand extends BaseCommand
 
     public function handle(): void
     {
-        echo '初始化脚本';
+        $this->line('Start initializing project...');
+        $this->init();
+    }
+
+    /**
+     * Create the domain directory.
+     *
+     * @return void
+     */
+    private function init(): void
+    {
+        $app    = app_path();
+        $this->backup($app, $app.'/backup');
+    }
+
+    /**
+     * @param $filename
+     * @param $backupName
+     * @return void
+     */
+    private function backup($filename, $backupName): void
+    {
+        if (!file_exists($filename)) {
+            throw new RuntimeException(sprintf('File "%s" does not exist', $filename));
+        }
+
+        if (!file_exists($backupName) && !mkdir($backupName, 0777, true) && !is_dir($backupName)) {
+            throw new RuntimeException(sprintf('Directory "%s" was not created', $backupName));
+        }
+
+        $files = scandir($filename);
+
+        foreach ($files as $file) {
+            if ($file === '.' || $file === '..') {
+                continue;
+            }
+
+            $sourcePath      = $filename.'/'.$file;
+            $destinationPath = $backupName.'/'.$file;
+
+            if (is_dir($sourcePath)) {
+                $this->backup($sourcePath, $destinationPath);
+            } else {
+                copy($sourcePath, $destinationPath);
+            }
+        }
+
     }
 }
