8e0f8dc793351d3ec331c55c6201da53ddffc8e7
小黑 <506711663@qq.com>
2024-05-11 22:15:08 +0800

    替换日志配置

--- config/ddd.php
+++ config/ddd.php
@@ -1,10 +1,31 @@
 <?php
 
 return [
-    'application' => [
-        "domain_name" => env('DOMAIN_NAME', 'Domains'),
-        'interface_name' => env('INTERFACE_NAME', 'Interfaces'),
-    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Domain directory name
+    |--------------------------------------------------------------------------
+    |
+    | Here you can configure the domain name directory name for the application.
+    | Provide personalized directory name customization for users. Default name domain
+    |
+    |
+    */
+    'domain_name' => env('DOMAIN_NAME', 'Domains'),
+    /*
+    |--------------------------------------------------------------------------
+    | System interface directory name
+    |--------------------------------------------------------------------------
+    |
+    | Here you can configure the domain name directory name for the application.
+    | Provide personalized directory name customization for users.
+    | This directory includes the program script directory and the controller directory.
+    | Default name interface
+    |
+    |
+    */
+    'interface_name' => env('INTERFACE_NAME', 'Interfaces'),
 
     'event' => [],
 
