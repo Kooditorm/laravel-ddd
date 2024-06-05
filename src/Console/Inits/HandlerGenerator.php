aeb939ea1874723abc57ddbe3f6df5ef28ebf9ba
小黑 <506711663@qq.com>
2024-06-05 22:31:54 +0800

    删除反射类

---
+++ src/Console/Inits/HandlerGenerator.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace DDDCore\Console\Inits\Stubs;
+
+use DDDCore\Libraries\Prettus\Generator;
+
+class HandlerGenerator extends Generator
+{
+
+    protected $stub = 'Handler';
+
+
+    public function getPathConfigNode(): string
+    {
+        return 'Handler';
+    }
+
+
+    public function getPath(): string
+    {
+        return $this->getBasePath().'/'.$this->getPathConfigNode().'/Handler.php';
+    }
+}
