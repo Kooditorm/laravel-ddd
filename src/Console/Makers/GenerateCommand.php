5e57d150d17de3ba8df3b14f1237b32963a42ae2
小黑 <506711663@qq.com>
2024-07-19 20:57:37 +0800

    更新底包服务

--- src/Console/Makers/GenerateCommand.php
+++ src/Console/Makers/GenerateCommand.php
@@ -4,6 +4,7 @@ namespace DDDCore\Console\Makers;
 
 use Illuminate\Console\Command;
 use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Str;
 
 
 /**
@@ -36,7 +37,9 @@ class GenerateCommand extends Command
     /**
      * @var array
      */
-    protected array $register = [];
+    protected array $register = [
+        DTOCommand::class
+    ];
 
 
     public function __construct(Filesystem $filesystem, ?string $name = null)
@@ -50,6 +53,82 @@ class GenerateCommand extends Command
 
     public function handle(): void
     {
+        //获取路径
+        $inputPath = Str::ucfirst(strtolower($this->argument('path')));
+        //获取表名
+        $table = $this->argument('table');
+        if ($table === Str::plural($table)) {
+            $table = Str::singular($table);
+        }
+        $tableName = Str::studly($table);
 
+        //定义操作参数
+        $option = [
+            'name'   => $tableName,
+            'action' => ''
+        ];
+        if ($this->option('del')) {
+            $option['action'] = 'delete';
+        }
+
+        //模块下
+        $basePath = app_path("Domain");
+        !$this->filesystem->exists($basePath) && $this->filesystem->makeDirectory($basePath);
+        $filePath = $basePath.DIRECTORY_SEPARATOR.$inputPath;
+        !$this->filesystem->exists($filePath) && $this->filesystem->makeDirectory($filePath);
+
+
+        //修改repository配置文件
+        config([
+            'repository.generator.basePath'          => $filePath,
+            'repository.generator.rootNamespace'     => "App\\Domain\\{$inputPath}\\",
+            'repository.generator.stubsOverridePath' => __dir__,
+            'repository.generator.inputPath'         => $inputPath
+        ]);
+
+
+        $isExe = true;
+
+        if ($option['action'] === 'delete') {
+            $isExe = $this->confirm('此操作删除 '.$tableName.' 表所删除的文件，您确定执行');
+        }
+
+        if ($isExe) {
+            $this->runCall($option);
+        }
+    }
+
+    private function runCall(array $option = []): void
+    {
+        $runCommand = [];
+        $commandObj = [];
+        $single     = 0;
+
+        $register = $this->register;
+
+        foreach ($register as $reg) {
+            if (array_key_exists($reg, $commandObj) === false) {
+                $obj = new $reg();
+                if (method_exists($obj, 'single')) {
+                    $commandObj[$reg] = [$obj->getName(), $obj->single()];
+                    if ($obj->single()) {
+                        $single++;
+                    }
+                } else {
+                    $commandObj[$reg] = [$obj->getName(), false];
+                }
+            }
+        }
+
+
+        if (!empty($commandObj)) {
+            foreach($commandObj as $command){
+                if ($command[1] === true){
+                    $option['residue'] = $single;
+                    $single--;
+                }
+                $this->call($command[0], $option);
+            }
+        }
     }
 }
