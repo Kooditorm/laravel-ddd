14a84099eb2d47871d9f0dd129b8dc2f0c2c431c
小黑 <506711663@qq.com>
2024-08-05 13:50:41 +0800

    更新底包服务

--- src/Providers/AbstractServiceProvider.php
+++ src/Providers/AbstractServiceProvider.php
@@ -3,7 +3,9 @@
 namespace DDDCore\Providers;
 
 use DDDCore\Facades\TraceChainId;
+use DDDCore\Middleware\LogsMiddleware;
 use DDDCore\Supports\JWT;
+use Illuminate\Foundation\Http\Kernel;
 use Illuminate\Support\ServiceProvider;
 
 /**
@@ -14,11 +16,31 @@ abstract class AbstractServiceProvider extends ServiceProvider
 {
 
     /**
+     * The application's global HTTP middleware stack.
+     *
+     * These middleware are run during every request to your application.
+     *
+     * @var array
+     */
+    protected array $middleware = [
+        LogsMiddleware::class
+    ];
+
+    /**
+     * The application's route middleware.
+     *
+     * These middleware may be assigned to groups or used individually.
+     *
+     * @var array
+     */
+    protected array $routeMiddleware = [];
+
+    /**
      * Boot the service provider.
      *
      * @return void
      */
-    abstract public function boot():void;
+    abstract public function boot(): void;
 
     /**
      * Register the service provider.
@@ -29,6 +51,7 @@ abstract class AbstractServiceProvider extends ServiceProvider
     {
         $this->registerJwtProvider();
         $this->registerTraceChainIdProvider();
+        $this->registerMiddleware();
     }
 
     /**
@@ -41,11 +64,34 @@ abstract class AbstractServiceProvider extends ServiceProvider
         });
     }
 
-    protected function registerTraceChainIdProvider():void
+    protected function registerTraceChainIdProvider(): void
     {
         $this->app->singleton('TraceChainId', function () {
             return new TraceChainId();
         });
+    }
+
+    /**
+     * Register the middleware.
+     *
+     * @return void
+     */
+    protected function registerMiddleware(): void
+    {
+        //注册全局中间件
+        $kernel = $this->app[Kernel::class];
+        foreach ($this->middleware as $middleware) {
+            $kernel->pushMiddleware($middleware);
+        }
+
+        //注册路由中间件
+        $router = $this->app['router'];
+
+        $method = method_exists($router, 'aliasMiddleware') ? 'aliasMiddleware' : 'middleware';
+
+        foreach ($this->routeMiddleware as $alias => $middleware) {
+            $router->$method($alias, $middleware);
+        }
     }
 
 }
